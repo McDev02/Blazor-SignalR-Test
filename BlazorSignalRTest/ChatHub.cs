443273b7326bbc8ecfe56495b36e6f3c242f0c8d
Kevin Scheitler <kevin@crowbox.de>
2022-10-13 18:09:18 +0200

    Initial

---
+++ BlazorSignalRTest/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRTest
+{
+	[Authorize]
+	public class ChatHub : Hub
+	{
+		public async Task SendMessage(string message)
+		{
+			await Clients.All.SendAsync("MESSAGE_RECEIVED", $"Server: {message}");
+		}
+	}
+}
